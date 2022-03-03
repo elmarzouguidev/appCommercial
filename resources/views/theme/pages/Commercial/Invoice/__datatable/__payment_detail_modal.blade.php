5be9326652f53f4aa8f0ec4c998d956c900957e3
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-03-03 15:26:53 +0100

    Push

--- resources/views/theme/pages/Commercial/Invoice/__datatable/__payment_detail_modal.blade.php
+++ resources/views/theme/pages/Commercial/Invoice/__datatable/__payment_detail_modal.blade.php
@@ -4,8 +4,8 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id=orderdetailsModalLabel">Règlement N° :
-                        {{ optional($invoice->bill)->full_number }}</h5>
+                     {{--<h5 class="modal-title" id=orderdetailsModalLabel">Règlement N° :
+                        optional($invoice->bill)->full_number }}</h5>--}}
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
