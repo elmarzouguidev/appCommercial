9ffa677019a462896691e31fe3d2573c6b98e04a
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2022-03-12 17:12:58 +0100

    update Estimate PDF Template

--- resources/views/theme/estimates_template/template1/index.blade.php
+++ resources/views/theme/estimates_template/template1/index.blade.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>{{ optional($estimate->client)->entreprise }} - {{ $estimate->estimate_date->format('d-m-Y') }}</title>
     <style>
+        @page {
+            margin: 60px 25px;
+        }
+
         body {
             font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
             text-align: center;
@@ -135,48 +140,82 @@
             }
         }
 
+        footer {
+            position: fixed;
+            bottom: -10px;
+            left: 0px;
+            right: 0px;
+            height: 50px;
+            /** Extra personal styles **/
+
+            color: white;
+            text-align: center;
+            line-height: 10px;
+        }
+
     </style>
 </head>
 
 <body>
-<div class="invoice-box">
-    <table>
-        <tr class="top">
-            <td colspan="4">
-                <table>
-                    <tr>
-                        <td class="title" style="text-align: center;">
-                            <img src="{{ $companyLogo }}"
-                                style="height: 60px"/>
-                        </td>
 
-                    </tr>
-                </table>
-            </td>
-        </tr>
+    <footer>
 
-        <tr class="information">
-            <td colspan="4">
-                <table>
-                    <tr>
-                        <td style="width: 50% ;">
-                            <strong>Client : {{ optional($estimate->client)->entreprise }}</strong> <br/>
-                            ICE : {{ optional($estimate->client)->ice }}<br/>
-                            Adresse : {{ optional($estimate->client)->addresse }} <br/>
+        <div style="text-align: center; color:#333; font-size: 11px !important;">
+            <p>{{ optional(getCompany())->name }}</p>
+            <p>
+                {{ optional(getCompany())->addresse }}
+                Tel : {{ optional(getCompany())->telephone }}
+                E-mail : {{ optional(getCompany())->email }}
+            </p>
+            <p>
+                -R.C:{{ optional(getCompany())->rc }}
+                -PATENTE:{{ optional(getCompany())->patente }}
+                -I.F:{{ optional(getCompany())->if }}
+                -CNSS:{{ optional(getCompany())->cnss }}
+                -ICE:{{ optional(getCompany())->ice }}
+            </p>
+        </div>
+        <div class="bott" style=" width: 100%;">
+        </div>
 
-                        </td>
-                        <td>
-                            <strong>DEVIS N° : {{ $estimate->code }}</strong><br/>
-                            Date : {{ $estimate->estimate_date->format('d-m-Y') }}<br/>
-                            {{--Date d'échéance : {{ $estimate->due_date }} --}}
-                        </td>
+    </footer>
+    <div class="invoice-box">
+        <table>
+            <tr class="top">
+                <td colspan="4">
+                    <table>
+                        <tr>
+                            <td class="title" style="text-align: center;">
+                                <img src="{{ $companyLogo }}" style="height: 60px" />
+                            </td>
 
-                    </tr>
-                </table>
-            </td>
-        </tr>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
 
-        {{--<tr class="heading">
+            <tr class="information">
+                <td colspan="4">
+                    <table>
+                        <tr>
+                            <td style="width: 50% ;">
+                                <strong>Client : {{ optional($estimate->client)->entreprise }}</strong> <br />
+                                ICE : {{ optional($estimate->client)->ice }}<br />
+                                Adresse : {{ optional($estimate->client)->addresse }} <br />
+
+                            </td>
+                            <td>
+                                <strong>DEVIS N° : {{ $estimate->code }}</strong><br />
+                                Date : {{ $estimate->estimate_date->format('d-m-Y') }}<br />
+                                {{-- Date d'échéance : {{ $estimate->due_date }} --}}
+                            </td>
+
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+
+            {{-- <tr class="heading">
             <td colspan="4">Réferences client</td>
 
         </tr>
@@ -186,66 +225,64 @@
 
                 BC : {{ $invoice->bc_code }}
             </td>
-        </tr>--}}
+        </tr> --}}
 
-        <tr class="heading">
-            <td>Désignation</td>
-            <td>Qté</td>
-            <td>P.U HT</td>
-            <td>Montant HT</td>
-        </tr>
+            <tr class="heading">
+                <td>Désignation</td>
+                <td>Qté</td>
+                <td>P.U HT</td>
+                <td>Montant HT</td>
+            </tr>
 
-        @foreach ($estimate->articles as $article)
+            @foreach ($estimate->articles as $article)
+                <tr class="item {{ $loop->last ? 'last' : '' }}">
+                    <td style="width: 55% ;">{{ $article->designation }}</td>
+                    <td>{{ $article->quantity }}</td>
+                    <td>{{ $article->formated_prix_unitaire }} DH</td>
+                    <td>{{ $article->formated_montant_ht }} DH</td>
+                </tr>
+            @endforeach
 
-            <tr class="item {{ $loop->last ? 'last' : '' }}">
-                <td style="width: 55% ;">{{ $article->designation }}</td>
-                <td>{{ $article->quantity }}</td>
-                <td>{{ $article->formated_prix_unitaire }} DH</td>
-                <td>{{ $article->formated_montant_ht }} DH</td>
-            </tr>
-        @endforeach
+            <div class="pricer">
+                <tr class="heading-price lefter">
+                    <td colspan="4">Montant HT : {{ $estimate->formated_price_ht }} DH</td>
+                </tr>
+                <tr class="heading-price lefter">
+                    <td colspan="4">Montant TVA : {{ $estimate->formated_total_tva }} DH</td>
+                </tr>
+                <tr class="heading-price lefter">
+                    <td colspan="4">Montant TTC : {{ $estimate->formated_price_total }} DH</td>
+                </tr>
+            </div>
 
-        <div class="pricer">
-            <tr class="heading-price lefter">
-                <td colspan="4">Montant HT : {{ $estimate->formated_price_ht }} DH</td>
-            </tr>
-            <tr class="heading-price lefter">
-                <td colspan="4">Montant TVA : {{ $estimate->formated_total_tva }} DH</td>
-            </tr>
-            <tr class="heading-price lefter">
-                <td colspan="4">Montant TTC : {{ $estimate->formated_price_total }} DH</td>
-            </tr>
+        </table>
+
+    </div>
+
+    @if (isset($estimate->condition_general))
+        <div style="text-align: left;font-size: 12px;color:black">
+            <p>Condition général</p>
+            <p>{{ $estimate->condition_general }}</p>
         </div>
+    @endif
 
-    </table>
 
-</div>
 
-@if(isset($estimate->condition_general))
-    <div style="text-align: left;font-size: 12px;color:black">
-        <p>Condition général</p>
-        <p>{{$estimate->condition_general}}</p>
-    </div>
-@endif
 
-<div style="position: fixed; bottom: 0; width: 100%;">
-    <div style="text-align: center; color:#333; font-size: 14px">
-        <p>{{ optional(getCompany())->name }}</p>
-        <p>
-            {{ optional(getCompany())->addresse }}
-            Tel : {{ optional(getCompany())->telephone }}
-            E-mail : {{ optional(getCompany())->email }}
-        </p>
-        <p>
-            -R.C:{{ optional(getCompany())->rc }}
-            -PATENTE:{{ optional(getCompany())->patente }}
-            -I.F:{{ optional(getCompany())->if }}
-            -CNSS:{{ optional(getCompany())->cnss }}
-            -ICE:{{ optional(getCompany())->ice }}
-        </p>
-    </div>
-</div>
+    <script type="text/php">
 
+        if (isset($pdf) && $PAGE_COUNT > 1) {
+            $text = "Page {PAGE_NUM} / {PAGE_COUNT}";
+            $size = 5;
+            $font = $fontMetrics->getFont("Verdana");
+            $width = $fontMetrics->get_text_width($text, $font, $size) / 2;
+            $x = ($pdf->get_width() - $width);
+            $y = $pdf->get_height() - 35;
+            $pdf->page_text($x, $y, $text, $font, $size);
+        }
+
+
+</script>
 </body>
 
 </html>
